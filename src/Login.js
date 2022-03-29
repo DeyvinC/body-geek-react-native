97714a754da65fba0d97dd106a8b980eda93308b
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-03-29 11:24:58 -0400

    routes working

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import { render } from "react-dom";
+import { View, Text } from "react-native";
+
+export default function Login () {
+       return(
+            <View>
+                <Text>Welcome to login</Text>
+            </View>
+        )
+
+}
+
---
+++ src/SignUp.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+
+export default function SignUp () {
+    return(
+        <View>
+            <Text>Signup page</Text>
+
+        </View>
+    )
+}
