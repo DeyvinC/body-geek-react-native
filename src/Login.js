6f4a45cebf0dfc15f2f420b854461e89dd2c5421
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-04-01 15:23:25 -0400

    added some styling to login page

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,63 @@
-import { render } from "react-dom";
-import { View, Text } from "react-native";
+import { StyleSheet} from 'react-native';
+import { View, TextInput, Text } from "react-native";
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function Login () {
        return(
-            <View>
-                <Text>Welcome to login</Text>
-            </View>
+           <>
+              <Text style={styles.header}> Body Geek</Text>
+            
+            <View style={styles.container}>
+                <View style={styles.inputView}>
+                    <TextInput
+                    style={styles.TextInput}
+                    placeholder="Enter Email"
+                    placeholderTextColor="white"
+                    onChangeText={(email) => setEmail(email)}
+                    />
+                </View>
+           
+                <View style={styles.inputView}>
+                    <TextInput
+                    style={styles.TextInput}
+                    placeholder=" Enter Password"
+                    placeholderTextColor="white"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                    />
+                </View>
+          </View>
+          </>
         )
-
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      },
+    inputView: {
+        backgroundColor: "#d4772a",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+      },
+      
+      TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+      },
+      header: {
+          fontSize: 40,
+          textAlign: 'center',
+          marginTop: 20,
+          backgroundColor: 'white'
+      }
+  });
+  
